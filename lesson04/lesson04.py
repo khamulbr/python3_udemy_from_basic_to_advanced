f0c3e7f7fa8021e01fd146996d869e5d73b1ae4b
Alessandro Dias <alessandro.valerio.dias@gmail.com>
2021-11-12 11:00:52 -0300

    added some data types examples

---
+++ lesson04/lesson04.py
@@ -0,0 +1,12 @@
+"""
+Data types
+str - strings
+int - signed integers
+float - floating point numbers
+bool - boolean
+"""
+
+print('Alessandro', type('Alessandro'))
+print(12, type(12))
+print(-95.9120, type(-95.9120))
+print(10 == 10, type(10 == 10))
